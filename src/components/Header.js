0dbe99ae57f1e39ddf39cb83429bad54b3bbd734
Danny Pham <dpham760@gmail.com>
2019-12-20 11:23:18 -0800

    Setup base of project with header.

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { makeStyles, AppBar, Toolbar, Typography, IconButton } from '@material-ui/core';
+import logo from '../resources/logo.png'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    paddingBottom: 2.5,
+  },
+  logo: {
+    width: '3.5vh',
+    height: '3.5vh'
+  },
+  bar: {
+    background: '#0269a4',
+    borderBottomStyle: 'solid',
+    borderBottomWidth: '2.5px',
+    borderColor: '#013F64'
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    fontFamily: 'Roboto',
+    flexGrow: 1,
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <AppBar position='static' className={classes.bar}>
+        <Toolbar>
+          <IconButton edge='start' className={classes.menuButton} color='inherit' aria-label='menu'>
+            <img className={classes.logo} src={logo} alt='Logo' />
+          </IconButton>
+          <Typography variant='h5' className={classes.title}>
+            Pastebin
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}

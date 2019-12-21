fd50be8e1f28f8aaffd78758f305617dceaa9b5e
Danny Pham <dpham760@gmail.com>
2019-12-21 11:28:12 -0800

    Added chip for paste metadata. Added button to create new paste when viewing one.

--- src/components/Paste.js
+++ src/components/Paste.js
@@ -1,29 +1,39 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import AceEditor from 'react-ace';
-import { Grid } from '@material-ui/core';
+import { Grid, Button, Chip } from '@material-ui/core';
 
 import 'ace-builds/src-noconflict/mode-java';
 import 'ace-builds/src-noconflict/mode-python';
 import 'ace-builds/src-noconflict/mode-c_cpp';
 import 'ace-builds/src-noconflict/mode-plain_text';
-import 'ace-builds/src-noconflict/theme-chaos';
+import 'ace-builds/src-noconflict/theme-dracula';
 
+const theme = 'dracula'
 class Paste extends Component {
   constructor(props) {
     super(props)
     this.state = {
       paste: {
-        date: null,
-        author: null,
-        text: null,
-        type: null
+        date: '',
+        author: '',
+        text: '',
+        type: '',
       },
+      new: false,
       editorHeight: '90vh',
       editorWidth: 'auto'
     }
     this.getPaste()
     this.onResize = this.onResize.bind(this)
+  }
+
+  onResize = (w, h) => {
+    this.setState({
+      editorWidth: w,
+      editorHeight: h
+    })
   }
 
   getPaste = () => {
@@ -43,57 +53,87 @@ class Paste extends Component {
               type: response.data.type
             }
           })
+        } else {
+          this.setState({
+            ...this.state,
+            paste: {
+              date: null,
+              author: null,
+              text: null,
+              type: null
+            }
+          })
         }
       }).catch(error => {
         // If there is an error, leave everything as is.
+        this.setState({
+          ...this.state,
+          paste: {
+            date: null,
+            author: null,
+            text: null,
+            type: null
+          }
+        })
       })
   }
 
-  onResize = (w, h) => {
+  redirectHome = () => {
     this.setState({
-      editorWidth: w,
-      editorHeight: h
+      ...this.state,
+      new: true
     })
   }
 
   render() {
-
-    if(this.state.paste.text !== null) {
-      return (
-        <Grid container>
-          <Grid item xs={4}>
-            Author: {this.state.paste.author}
+    if(!this.state.new) {
+      if(this.state.paste.text !== null) {
+        return (
+          <Grid container>
+            <Grid item xs={6} style={{ paddingLeft: '0.5%', paddingTop: '0.5%' }} >
+              <Chip label={this.state.paste.author} />
+              <Chip label={this.state.paste.date} style={{ marginLeft: '0.5%', marginRight: '0.5%' }} />
+              <Chip label={this.state.paste.type} />
+            </Grid>
+            <Grid item xs={6} style={{ textAlign: 'right', paddingRight: '0.5%', paddingTop: '0.5%' }} >
+              <Button 
+                variant='contained' color='primary' 
+                onClick={this.redirectHome} 
+                style={{ background: '#0269a4' }}>
+                Create New Paste
+              </Button>
+            </Grid>
+            <Grid item xs={12} style={{ margin: '0.5%' }} >
+              <AceEditor
+                name='paste'
+                theme={theme}
+                height={this.state.editorHeight}
+                width={this.state.editorWidth}
+                value={this.state.paste.text}
+                mode={this.state.paste.type}
+                showGutter={false}
+                showPrintMargin={false}
+                editorProps={{ $blockScrolling: true }}
+                cursorStart={0}
+                showLineNumbers={false}
+                readOnly={true}
+                highlightActiveLine={false}
+                setOptions={{
+                  showLineNumbers: false
+                }}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={4}>
-            Date posted: {this.state.paste.date}
-          </Grid>
-          <Grid item xs={12}>
-            <AceEditor
-              name='paste'
-              theme='chaos'
-              height={this.state.editorHeight}
-              width={this.state.editorWidth}
-              value={this.state.paste.text}
-              mode={this.state.paste.type}
-              showGutter={false}
-              showPrintMargin={false}
-              editorProps={{ $blockScrolling: true }}
-              cursorStart={0}
-              showLineNumbers={false}
-              readOnly={true}
-              highlightActiveLine={false}
-              setOptions={{
-                showLineNumbers: false
-              }}
-            />
-          </Grid>
-        </Grid>
-      )
+        )
+      } else {
+        // TODO: Have a better UI to display that a paste was not found under this ID.
+        return (
+          <p>There was no data found!</p>
+        )
+      }
     } else {
-      // TODO: Have a better UI to display that a paste was not found under this ID.
-      return (
-        <p>There was no data found!</p>
-      )
+      const redirect_path = '/'
+      return <Redirect to={{ pathname: redirect_path }} />
     }
   }
 }
